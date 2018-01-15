3f2f897defafcb1ea7ecb3aec557e6e3620f6dfb
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-01-15 12:07:45 +0200

    Proxy calls to other 'go' binaries

--- bin/go.js
+++ bin/go.js
@@ -1,29 +1,49 @@
 #!/usr/bin/env node
 
+/*
+ * TODO:
+ * - all commands should be registered with information that will be used to generate HELP output
+ * - after loading template the default command should be runned if it is presented
+ */
+
 var fs = require('fs')
 var path = require('path')
 var exec = require('shelljs').exec
+var findNextGoBinary = require('../lib/find-next-go-binary')
 
-function exit (error) {
-  if (error) console.error('Go [error]: ' + error.toString())
-  process.exit(1)
+var ERROR_CODE = 1
+var COMMAND_NOT_FOUND_CODE = 44
+
+function triggerError (msg, code) {
+  if (!code) code = ERROR_CODE
+  console.error('Go [error]:', msg)
+  process.exit(ERROR_CODE)
 }
 
 var goConfigFile = require('../lib/find-goconfig')()
 if (!goConfigFile) {
-  exit(new ReferenceError('go.config.js file is not found in current and parent folders'))
+  return triggerError('go.config.js file is not found in current and parent folders')
 }
 
-var command = process.argv.slice(2).join(' ')
+var argsString = process.argv.slice(2).join(' ')
 
 var entryScript = fs.readFileSync(path.resolve(__dirname, '../scripts/run-goconfig.js'))
   .toString()
   .replace(/%gocli-dir%/g, path.resolve(__dirname, '..'))
   .replace(/%goconfig-file%/g, goConfigFile)
   // TODO: Make sure ' and " works fine after replacing
-  .replace(/%command%/g, command)
+  .replace(/%command%/g, argsString)
 
 var shellCommand = 'node -e "' + entryScript + '"'
 var exitCode = exec(shellCommand, { cwd: path.dirname(goConfigFile) }).code
 
-if (exitCode) process.exit(exitCode)
+if (exitCode === COMMAND_NOT_FOUND_CODE) {
+  var binary = findNextGoBinary()
+  if (!binary) {
+    return triggerError('unknown command was triggered: go ' + argsString, exitCode)
+  }
+  exitCode = exec([binary, argsString].join(' ')).code
+  return process.exit(exitCode)
+}
+
+if (exitCode) triggerError('excution has failed', exitCode)
--- scripts/run-goconfig.js
+++ scripts/run-goconfig.js
@@ -1,3 +1,6 @@
+var ERROR_CODE = 1
+var COMMAND_NOT_FOUND_CODE = 44
+
 var go = require('go')
   .use(require('%gocli-dir%/plugins/go-commands'))
   .use(require('%gocli-dir%/plugins/github'))
@@ -9,6 +12,6 @@ go.executeCommand('%command%')
     if (typeof result !== 'undefined') console.log(result)
   })
   .catch(error => {
-    console.error('Go [error]: ' + error.toString())
-    process.exit(1)
+    var exitCode = error instanceof ReferenceError ? COMMAND_NOT_FOUND_CODE : ERROR_CODE
+    process.exit(exitCode)
   })
