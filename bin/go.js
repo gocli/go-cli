aeb25fe8ebd6acd20cb887084b6cda60a9a8e3a7
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-01-20 10:49:28 +0200

    Run install after loading template

--- bin/go.js
+++ bin/go.js
@@ -3,21 +3,24 @@
 /*
  * TODO:
  * - all commands should be registered with information that will be used to generate HELP output
- * - after loading template the default command should be runned if it is presented (using .goconfig file)
  * - add help output
  * - add completions
  */
 
 var fs = require('fs')
+var spawn = require('child_process').spawn
 var Liftoff = require('liftoff')
 var minimist = require('minimist')
 var interpret = require('interpret')
 
 var OK = 0
 var ERROR = 1
+var CONFIG_FILE = '.goconfig'
 
 var args = process.argv.slice(2)
-var argv = minimist(args)
+var argv = minimist(args, {
+  default: { install: true }
+})
 
 var Cli = new Liftoff({
   name: 'go',
@@ -33,9 +36,9 @@ Cli.launch({
 }, execute)
 
 function execute (env) {
-  if (!args[0]) exit('Help is not ready yet', ERROR)
+  if (!argv._[0]) exit('Help is not ready yet', ERROR)
 
-  if (args[0] && args[0].indexOf(':') === 0) {
+  if (isInnerCommand(argv)) {
     return executeInnerCommand(argv)
   }
 
@@ -43,6 +46,10 @@ function execute (env) {
   if (!env.configPath) exit('Gofile is not found', ERROR)
 
   executeLocalCommand(env.modulePath, env.configPath, argv)
+}
+
+function isInnerCommand (argv) {
+  return argv._[0] && argv._[0].indexOf(':') === 0
 }
 
 function executeInnerCommand (argv) {
@@ -53,13 +60,18 @@ function executeInnerCommand (argv) {
 
   if (!loader) exit(loaderName + ' loader is not registered', ERROR)
 
-  loader(source, argv)
+  return loader(source, argv)
     .then(function (destination) {
-      exit('project is deployed to the directory \`' + destination + '\`')
+      function finish () {
+        exit('project is deployed to the directory \`' + destination + '\`')
+      }
+
+      if (!argv.install) finish()
+      else installTemplate(destination)
+        .then(finish)
+        .catch(function (error) { exit(error, ERROR) })
     })
-    .catch(function(error) {
-      exit(error, ERROR)
-    })
+    .catch(function(error) { exit(error, ERROR) })
 }
 
 function getLoader (name) {
@@ -68,6 +80,30 @@ function getLoader (name) {
   } catch (err) {
     return null
   }
+}
+
+function installTemplate (destination) {
+  return new Promise(function (resolve, reject) {
+    var configPath = destination + '/' + CONFIG_FILE
+
+    if (!fs.statSync(configPath)) return resolve('file not found')
+
+    try {
+      var config = require(configPath)
+      if (!config) return resolve()
+
+      if (config.install) {
+        spawn(config.install, { stdio: 'inherit', shell: true, cwd: destination })
+          .on('error', reject)
+          .on('exit', function (code) {
+            if (code) reject('install command has failed')
+            else resolve()
+          })
+      }
+    } catch (err) {
+      reject(err)
+    }
+  })
 }
 
 function executeLocalCommand (modulePath, configPath, argv) {
