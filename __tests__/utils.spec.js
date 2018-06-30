6d0808da786383551ef2eff72a58c9bd8fa33e93
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-06-30 17:26:48 +0300

    cover utils with tests

--- __tests__/utils.spec.js
+++ __tests__/utils.spec.js
@@ -1,12 +1,42 @@
-const { normalizeLoaderName } = require('../lib/utils')
+const mockResolveGlobal = () => mockResolveGlobal.dependency
+mockResolveGlobal.silent = () => mockResolveGlobal.dependency
+mockResolveGlobal.dependency = null
+jest.mock('resolve-global', () => mockResolveGlobal)
+
+const { normalizeLoaderName, requireLoader } = require('../lib/utils')
 
 describe('Utils', () => {
+
+  beforeEach(() => {
+    mockResolveGlobal.dependency = null
+  })
+
   describe('normalizeLoaderName', () => {
     it('formats the given name', () => {
       expect(normalizeLoaderName('git')).toBe('go-loader-git')
       expect(normalizeLoaderName(' GitHub ')).toBe('go-loader-github')
+    })
+
+    it('handles non string arguments', () => {
       expect(normalizeLoaderName()).toBe(null)
       expect(normalizeLoaderName(true)).toBe(null)
+    })
+  })
+
+  describe('requireLoader', () => {
+    it('requires local dependency', () => {
+      const dep = 'path'
+      expect(requireLoader(dep)).toBe(require(dep))
+    })
+
+    it('requires global dependency', () => {
+      const dep = 'path'
+      mockResolveGlobal.dependency = dep
+      expect(requireLoader('global-dependency')).toBe(require(dep))
+    })
+
+    it('returns null if dependency is not matched', () => {
+      expect(requireLoader('missing-dependency')).toBe(null)
     })
   })
 })
